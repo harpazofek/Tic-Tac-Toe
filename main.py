a750d01da2631946a3a1c0fc4635b480fc101ecc
ofek.harapz <oharpaz1@gmail.com>
2023-03-17 00:43:36 +0200

    push

---
+++ main.py
@@ -0,0 +1,115 @@
+import math
+import functions
+
+
+
+# # Game TIC TAC TOE:
+
+
+# # engen_command = ""
+
+# # while True:
+# #     engen_command = engen_command =  print(str.lower(input("> ")))
+# #     if engen_command == "start":
+# #         print("Engen Started")
+# #     elif engen_command == "stop":
+# #         print("Engen car stoped")
+# #     elif engen_command == "help":
+# #         print(""" "Start - to start the car"\n"Stop - to stop the car"\n"Quit -to exit" """)
+# #     elif engen_command == "quit":
+# #         print("Engen Program Shoutdown")
+# #         break
+# #     else:
+# #         print("This command not approved")
+
+
+
+# # def func_3X3(table):
+
+
+# # def game_menu(lobby):
+# #     game_menu = ""
+
+# game_menu = ""
+
+print("Hello user this is tha main menu for Tic-Tac-Toe game chose of the options : ")
+
+# # while True:
+# #     game_menu =  print(str.lower(input("'Which board do you prefer to play on? \n('1'): "3X3" \n('2'): "4X4" '")))
+# #     if game_menu == 1:
+# #         print("func_3X3")
+# #     elif game_menu == 2:
+# #         print("4X4 Table")
+# #     # elif game_menu == "help":
+# #     #     print(""" "Start - to start the car"\n"Stop - to stop the car"\n"Quit -to exit" """)
+# #     elif game_menu == "quit":
+# #         print("Engen Program Shoutdown")
+# #         break
+# #     else:
+# #         print("This command not approved")
+
+# # command = ""
+# # started = False
+# # while True:
+# #     command = str.lower(input("> "))
+# #     if command == "start":
+# #         if started:
+# #             print("Engen alredy started")
+# #         else:
+# #             started = True
+# #             print("Car started...")
+# #     elif command == "stop":
+# #         if not started:
+# #             print("Car is alredy stopped")
+# #         else:
+# #             started = False
+# #             print("Engen Stoped")
+# #     elif command == "help":
+# #         print('"Start - to start the car"\n"Stop - to stop the car"\n"Quit -to exit"')
+# #     elif command == "quit":
+# #         print("Engen program exit!")
+# #         break
+# #     else:
+# #         print("This command not approved")
+
+
+# # -------------------------------------------------------------------
+
+# # price_list = [10, 20, 30]
+# # total = 0
+# # for total_cust in price_list:
+# #     total += total_cust
+# # print(f"Total : {total}")
+
+# # x = "x"
+# # number = {5, 2, 5, 2, 2}
+# # for x in range(number):
+# #     print(f'{number}'*'  ')
+
+
+# # number = list[5, 2, 5, 2, 2]
+# # for x in range(2):
+# #     x = "x" * 5
+# #     print(f'({x})')
+# #     for y in range(3):
+# #         y = "x" * 2
+# #         print(f'({y})')
+#         # for y in range(2):
+#         #     y = "x" * 2
+#         #     print(f'({y})')
+#         # for z in range(3):
+#         #     z = "x" * 5
+#         #     print(f'({z})')
+#         #     for f in range(4):
+#         #             f = "x" * 2
+#         #             print(f'({f})')
+#         #             for t in range(5):
+#         #                 t = "x" * 2
+#         #                 print(f'({t})')
+
+# # number = [2, 2, 2, 2, 15]
+# # for x_cunt in number:
+# #     output = ''
+# #     for count in range(x_cunt):
+# #         output += 'x'
+# #     print(output)
