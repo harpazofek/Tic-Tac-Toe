d751b398468e56152f1916f1b875f6837dd3fc38
ofek.harapz <oharpaz1@gmail.com>
2023-03-17 02:35:06 +0200

    few changes

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ import functions
 print("Hello user this is tha main menu for Tic-Tac-Toe game chose of the options : ")
 
 # Game Settings
-
-# from hellow_user import wellcom
-
-from functions import game_settings # this dunction bring the game menu to stdout
+from functions import game_settings # this function set the game menu to stdout
 
 # Player Icon: "X" or "O"
-from playericon import icon # plyer icon like "X" or "Y"
+from playericon import icon_slected # plyer icon like "X" or "O"
 
 # from tables import table_graphics # this function set the size of the table "3X3" or "4X4"
+# from tables import table_size
